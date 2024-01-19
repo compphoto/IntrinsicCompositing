a9e827cbbe44d4d39ae54383156a9f3c45ce536d
Utkarsh Doshi <79432685+utkd135@users.noreply.github.com>
2024-01-19 13:23:08 +0530

    Update pipeline.py
    in line 16 it should be elif instead of just if

--- intrinsic_compositing/shading/pipeline.py
+++ intrinsic_compositing/shading/pipeline.py
@@ -13,7 +13,7 @@ def load_reshading_model(path, device='cuda'):
     
     if path == 'paper_weights':
         state_dict = torch.hub.load_state_dict_from_url('https://github.com/compphoto/IntrinsicCompositing/releases/download/1.0.0/shading_paper_weights.pt', map_location=device, progress=True)
-    if path == 'further_trained':
+    elif path == 'further_trained':
         state_dict = torch.hub.load_state_dict_from_url('https://github.com/compphoto/IntrinsicCompositing/releases/download/1.0.0/further_trained.pt', map_location=device, progress=True)
     else:
         state_dict = torch.load(path) 
